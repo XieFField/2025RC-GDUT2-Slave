ff206fb47541344f67660de23d1ee24fc7470f66
XieFField <3432674139@qq.com>
2025-05-24 10:26:28 +0800

    从机架构

--- Slave/Task/User/Src/fsm_joy.cpp
+++ Slave/Task/User/Src/fsm_joy.cpp
@@ -204,63 +204,6 @@ void Air_Joy_Task(void *pvParameters)
                 ctrl.car_comm_ctrl = CAR_COMMUICA_OFF;
                 ctrl.laser_ctrl = LASER_CALIBRA_OFF;
             }
-
-
-            //底盘控制命令
-            // if(air_joy.SWA>1950&&air_joy.SWA<2050)
-            // {                
-            //     ctrl.twist.linear.y = -(air_joy.LEFT_Y - 1500)/500.0 * 3;
-            //     ctrl.twist.linear.x = -(air_joy.LEFT_X - 1500)/500.0 * 3;
-            //     ctrl.twist.angular.z = (air_joy.RIGHT_X - 1500)/500.0 * 2;
-
-            //     ctrl.twist.angular.x = air_joy.RIGHT_Y;
-
-            //     ctrl.twist.pitch.column = (air_joy.RIGHT_Y - 1500)/500.0 * 2;
-
-            //     if(_tool_Abs(air_joy.SWD - 2000)<50)    //手动俯仰，俯仰时禁止底盘运动
-            //     {
-            //         ctrl.pitch_ctrl = PITCH_HAND;
-            //         ctrl.chassis_ctrl = CHASSIS_OFF;
-            //         if(_tool_Abs(air_joy.SWC-1500)<50)      //开启摩擦轮
-            //         {
-            //             ctrl.friction_ctrl = FRICTION_ON;
-            //         }
-            //         else if(_tool_Abs(air_joy.SWC-2000)<50)
-            //         {
-            //             ctrl.friction_ctrl = FRICTION_ON;   //开摩擦轮
-            //             ctrl.shoot_ctrl = SHOOT_ON;         //启动推杆，射球
-            //         }
-            //         else if(_tool_Abs(air_joy.SWB - 1500) < 50 )
-            //         {
-            //             ctrl.pitch_ctrl = PITCH_ATUO1;
-            //         }
-            //         else if(_tool_Abs(air_joy.SWB - 2000) < 50)
-            //         {
-            //             ctrl.pitch_ctrl = PITCH_ATUO2;
-            //         }
-            //         else
-            //         {
-            //             ctrl.shoot_ctrl = SHOOT_OFF;        //推杆复位
-            //             ctrl.friction_ctrl = FRICTION_OFF;  //关摩擦轮
-            //         }
-
-                    
-            //     }
-            //     else    
-            //     {
-            //         ctrl.shoot_ctrl = SHOOT_OFF;    //推杆复位
-            //         ctrl.friction_ctrl = FRICTION_OFF;
-            //         ctrl.pitch_ctrl = PITCH_LOCK;
-            //         ctrl.chassis_ctrl = CHASSIS_ON; 
-            //     }
-            // }
-			// else
-			// {
-            //     ctrl.chassis_ctrl = CHASSIS_OFF;
-            //     ctrl.friction_ctrl = FRICTION_OFF;
-            //     ctrl.pitch_ctrl = PITCH_RESET;
-            //     ctrl.shoot_ctrl = SHOOT_OFF;
-			// }
             xQueueSend(Chassia_Port, &ctrl, 0);
         }
         else
